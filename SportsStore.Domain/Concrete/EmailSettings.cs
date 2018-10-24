f36d2363389fdffdd22a006950697f729e6e3949
kseniia-dziuba-NURE <kseniia.dziuba@nure.ua>
2018-10-24 23:47:16 +0300

    adding email settings

---
+++ SportsStore.Domain/Concrete/EmailSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsStore.Domain.Concrete
+{
+    public class EmailSettings
+    {
+        public string MailToAddress = "ksenia110899@gmail.com";
+    }
+}
